301edec27730424d2abe3e01b8723c8c7583076f
Juan Alberto <juan.selfaleal@gmail.com>
2021-05-07 23:03:38 +0200

    Añadido código a los ficheros estáticos y modificaciones en los returns del server para que cargue desde el template y no desde la página estática.

--- server.py
+++ server.py
@@ -1,27 +1,27 @@
 # -*- coding: iso-8859-15 -*-
 
-
+from flask import render_template
 from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route('/home', methods=['GET'])
 def home():
-    return app.send_static_file('home.html')
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET'])
 def login():
-    return app.send_static_file('login.html')
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    return app.send_static_file('signup.html')
+    return render_template('signup.html')
 
 
 @app.route('/processLogin', methods=['GET', 'POST'])
