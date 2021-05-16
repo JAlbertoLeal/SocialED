032f28cd637f5c9596524c7db70951aad87ed9ae
Adrian Bravo <bravoacm96@gmail.com>
2021-05-16 19:56:37 +0200

    añado modificaciones a server.py y realizo varias pruebas

---
+++ data/bravoacm96@gmail.com
@@ -0,0 +1,1 @@
+{"user_name": "bravoacm", "password": "adrian", "messages": [], "friends": []}
--- server.py
+++ server.py
@@ -1,50 +1,55 @@
 # -*- coding: iso-8859-15 -*-
 
+import json
+from flask import Flask, request, render_template, session, redirect, url_for
+import os.path
+from os import listdir
+import json
+from time import time
+import sys
 
-from flask import Flask, request
-from flask import render_template
 app = Flask(__name__)
 
-# este codigo controla los errores de campos ausentes
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
 def process_missingFields(campos, next_page):
-    """
-    :param campos: Lista de Campos que faltan
-    :param next_page: ruta al pulsar botón continuar
-    :return: plantilla generada
-    """
     return render_template("missingFields.html", inputs=campos, next=next_page)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    if 'user_name' in session:
+        return render_template("index.html", logged=True, nickname=session['user_name'])
+    else:
+        return render_template("index.html", logged=False, nickname='')
 
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template("home.html")
+    return render_template('home.html', logged=True, nickname=session['user_name'], messages=session['messages'])
 
 @app.route('/login', methods=['GET'])
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    return render_template("signup.html")
+    return render_template('signup.html')
 
 
 @app.route('/processLogin', methods=['GET', 'POST'])
 def processLogin():
-       missing = []
-       fields = ['email', 'passwd', 'login_submit']
-       for field in fields:
-              value = request.form.get(field, None)
-              if value is None:
-                  missing.append(field)
-       if missing:
-              return process_missingFields(missing, "/login")
+    missing = []
+    fields = ['email', 'passwd', 'login_submit']
+    for field in fields:
+        value = request.form.get(field, None)
+        if value is None:
+            missing.append(field)
+    if missing:
+        return process_missingFields(missing, "/login")
+    return load_user(request.form['email'], request.form['passwd'])
 
-
+"""
        return '<!DOCTYPE html> ' \
            '<html lang="es">' \
            '<head>' \
@@ -58,19 +63,21 @@ def processLogin():
 	       '</label><br><label>passwd: ' + request.form['passwd'] + \
            '</label></form></div></body>' \
            '</html>'
-
+"""
 
 @app.route('/processSignup', methods=['GET', 'POST'])
 def processSignup():
-       missing = []
-       fields = ['nickname', 'email', 'passwd','confirm', 'signup_submit']
-       for field in fields:
-              value = request.form.get(field, None)
-              if value is None:
-                     missing.append(field)
-       if missing:
-              return process_missingFields(missing, "/signup")
+    missing = []
+    fields = ['nickname', 'email', 'passwd','confirm', 'signup_submit']
+    for field in fields:
+        value = request.form.get(field, None)
+        if value is None:
+            missing.append(field)
+    if missing:
+        return process_missingFields(missing, "/signup")
+    return create_user_file(request.form['nickname'], request.form['email'], request.form['passwd'], request.form['confirm'])
 
+"""
        return '<!DOCTYPE html> ' \
            '<html lang="es">' \
            '<head>' \
@@ -86,43 +93,79 @@ def processSignup():
 	       '</label><br><label>confirm: ' + request.form['confirm'] + \
            '</label></form></div></body>' \
            '</html>'
-
+"""
 
 @app.route('/processHome', methods=['GET', 'POST'])
 def processHome():
-	missing = []
-	fields = ['message', 'last', 'post_submit']
-	for field in fields:
-		value = request.form.get(field, None)
-		if value is None:
-			missing.append(field)
-	if missing:
-		return process_missingFields(missing, "/home")
+    if 'user_name' not in session:
+        return process_error("you must be logged to use the app / debe registrarse antes de usar la aplicacion", url_for("login"))
 
-	return '<!DOCTYPE html> ' \
-           '<html lang="es">' \
-           '<head>' \
-           '<link href="static/css/socialed-style.css" rel="stylesheet" type="text/css"/>' \
-           '<title> Inicio - SocialED </title>' \
-           '</head>' \
-           '<body> <div id="container">' \
-		   '<a href="/"> SocialED </a> | <a href="home"> Home </a> | <a href="login"> Log In </a> | <a href="signup"> Sign Up </a>' \
-           '<h1>Hi, How are you?</h1>' \
-                	'<form action="processHome" method="post" name="home"> ' \
-			'<label for="message">Say something:</label><div class="inputs">' \
-			'<input id="message" maxlength="128" name="message" size="80" type="text" required="true" value=""/>' \
-			'<input id="last" type="hidden" name="last" required="true" value="' + request.form['last'] + '<br>'+ request.form['message'] + '">' \
-	                 '</div>' \
-                    	'<div class="inputs">' \
-                        '<input id="post_submit" name="post_submit" type="submit" value="Post!"/>' \
-           		'<br><br>Previous Posts: <br>' + request.form['last'] + '<br>' +request.form['message'] + \
-                	'</form>' \
-            		'</div></div>' \
-           '</body>' \
-           '</html>'
+    if request.method == 'POST' and request.form['message'] !="":
+        messages=session['messages']
+        if not messages:
+            messages=[]
+        messages.append((time(), request.form['message']))
+        save_current_user()
+    else:
+        messages=session['messages']
+    session['messages']=messages
+    return render_template('home.html', logged=True, nickname=session['user_name'], messages=messages)
 
+def process_error(message, next_page):
+    return render_template("error.html", error_message=message, next=next_page)
 
-#app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+def load_user(email, passwd):
+    file_path = os.path.join(SITE_ROOT, "data/", email)
+    if not os.path.isfile(file_path):
+        return process_error("User not found / No existe un usuario con ese nombre", url_for("login"))
+    with open(file_path, 'r') as f:
+        data=json.load(f)
+    if data['password'] != passwd:
+        return process_error("Incorrect password / La clave no es correcta", url_for("login"))
+    session['user_name']=data['user_name']
+    session['messages']=data['messages']
+    session['password']=passwd
+    session['email']=email
+    session['friends']=data['friends']
+    return render_template('home.html', logged=True, nickname=session['user_name'], messages=session['messages'])
+
+def save_current_user():
+    datos={
+        "user_name": session["user_name"],
+        "password": session['password'],
+        "messages": session['messages'],
+        "email": session['email'],
+        "friends": session['friends']
+    }
+    file_path = os.path.join(SITE_ROOT, "data/", session['email'])
+    with open(file_path, 'w') as f:
+        json.dump(datos, f)
+
+def create_user_file(name, email, passwd, passwd_confirmation):
+    directory=os.path.join(SITE_ROOT, "data")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path=os.path.join(SITE_ROOT, "data/", email)
+    if os.path.isfile(file_path):
+        return process_error("The email is already used, you must select a different email / Ya existe un usuario con ese email", url_for("signup"))
+    if passwd != passwd_confirmation:
+        return process_error("Your password and confirmation password do not match / Las claves no coinciden", url_for("signup"))
+    datos={
+        "user_name": name,
+        "password": passwd,
+        "messages": [],
+        "friends": []
+    }
+    with open(file_path, 'w') as f:
+        json.dump(datos, f)
+    session['user_name']=name
+    session['password']=passwd
+    session['messages']=[]
+    session['friends']=[]
+    session['email']=email
+    return render_template('home.html', logged=True, nickname=session['user_name'], messages=session['messages'])
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True, port=55555)
