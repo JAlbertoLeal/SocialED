78f070767083962267c7f6e6c80de50c5cf0b254
Juan Alberto <juan.selfaleal@gmail.com>
2021-05-16 16:51:13 +0200

    Ajustes

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
 # -*- coding: iso-8859-15 -*-
 
 
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, render_template, session, redirect, url_for
+import os.path
+from os import listdir
+import json
+from time import time
+import sys
+
 app = Flask(__name__)
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+
+@app.route('/', methods=['GET'])
+
+@app.route('/index.html', methods=['GET'])
+def index():
+    """
+    Procesa '/' y '/index' urls.
+    :return: contenido index.html
+    """
+    if 'user_name' in session:
+        logged = True
+        nickname = session['user_name']
+    else:
+        logged = False
+        nickname = ''
+    return render_template('index.html', logged=logged, nickname=nickname)
+
 
 # este codigo controla los errores de campos ausentes
 def process_missingFields(campos, next_page):
@@ -14,23 +39,28 @@ def process_missingFields(campos, next_page):
     """
     return render_template("missingFields.html", inputs=campos, next=next_page)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/home.html', methods=['GET'])
 def home():
     return render_template("home.html")
 
-@app.route('/login', methods=['GET'])
+@app.route('/login.html', methods=['GET'])
 def login():
     return render_template("login.html")
 
 
-@app.route('/signup', methods=['GET'])
+@app.route('/signup.html', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    """
+    '/signup' url (crear un usuario nuevo)
+    :return: Primero renderiza la página vacia. Después los datos.
+    """
+    if request.method == 'POST':
+        return process_signup()
+
+    # The http GET method was used
+    return app.send_static_file('signup.html')
 
 
 @app.route('/processLogin', methods=['GET', 'POST'])
@@ -42,7 +72,7 @@ def processLogin():
               if value is None:
                   missing.append(field)
        if missing:
-              return process_missingFields(missing, "/login")
+              return process_missingFields(missing, "/login.html")
 
 
        return '<!DOCTYPE html> ' \
@@ -69,7 +99,7 @@ def processSignup():
               if value is None:
                      missing.append(field)
        if missing:
-              return process_missingFields(missing, "/signup")
+              return process_missingFields(missing, "/signup.html")
 
        return '<!DOCTYPE html> ' \
            '<html lang="es">' \
@@ -97,7 +127,7 @@ def processHome():
 		if value is None:
 			missing.append(field)
 	if missing:
-		return process_missingFields(missing, "/home")
+		return process_missingFields(missing, "/home.html")
 
 	return '<!DOCTYPE html> ' \
            '<html lang="es">' \
@@ -121,6 +151,140 @@ def processHome():
            '</body>' \
            '</html>'
 
+
+def process_signup():
+    faltan = []
+    campos = ['nickname', 'email', 'passwd', 'confirm', 'signup_submit']
+    for campo in campos:
+        value = request.form.get(campo, None)
+        if value is None or value == '':
+            faltan.append(campo)
+    if faltan:
+        return render_template("missingFields.html", inputs=faltan, next=url_for("signup"))
+    return create_user_file(request.form['nickname'], request.form['email'], request.form['passwd'],
+                            request.form['confirm'])
+
+def process_error(message, next_page):
+    """
+
+    :param message:
+    :param next_page:
+    :return:
+    """
+    return render_template("error.html", error_message=message, next=next_page)
+
+
+def load_user(email, passwd):
+    """
+    Carga usuarios.
+    Comprueba que coincide el email
+    :param email: user id
+    :param passwd: password
+    :return: pagina inicio  si el usuario existe y el pass es correcto
+    """
+    file_path = os.path.join(SITE_ROOT, "data/", email)
+    if not os.path.isfile(file_path):
+        return process_error("User not found / No existe un usuario con ese nombre", url_for("login"))
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    if data['password'] != passwd:
+        return process_error("Incorrect password / la clave no es correcta", url_for("login"))
+    session['user_name'] = data['user_name']
+    session['messages'] = data['messages']
+    session['password'] = passwd
+    session['email'] = email
+    session['friends'] = data['friends']
+    return redirect(url_for("home"))
+
+def save_current_user():
+    datos = {
+        "user_name": session["user_name"],
+        "password": session['password'],
+        "messages": session['messages'],  # lista de tuplas (time_stamp, mensaje)
+        "email": session['email'],
+        "friends": session['friends']
+    }
+    file_path = os.path.join(SITE_ROOT, "data/", session['email'])
+    with open(file_path, 'w') as f:
+        json.dump(datos, f)
+
+
+def create_user_file(name, email, passwd, passwd_confirmation):
+    """
+    Crea el fichero (en /data ) el nombre del fichero es email.
+    Si ya existe el fichero error.
+    Si no coincide el pass error.
+    :param name: Nombre o nickname del usuario
+    :param email: correo, luego se usa para almacenar datos
+    :param passwd: password
+    :param passwd_confirmation: repite el pass
+    :return: Si no hay errores, muestra la pagina home
+    """
+
+    directory = os.path.join(SITE_ROOT, "data")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(SITE_ROOT, "data/", email)
+    if os.path.isfile(file_path):
+        return process_error(
+            "The email is already used, you must select a different email / Ya existe un usuario con ese nombre",
+            url_for("signup"))
+    if passwd != passwd_confirmation:
+        return process_error("Your password and confirmation password do not match / Las claves no coinciden",
+                             url_for("signup"))
+    datos = {
+        "user_name": name,
+        "password": passwd,
+        "messages": [],
+        "friends": []
+    }
+    with open(file_path, 'w') as f:
+        json.dump(datos, f)
+    session['user_name'] = name
+    session['password'] = passwd
+    session['messages'] = []
+    session['friends'] = []
+    session['email'] = email
+    return redirect(url_for("home"))
+
+
+def get_friends_messages_with_authors():
+    """
+    Obtiene los mensajes de los amigos  (del usuario de la sesión)
+    :return: Lista de mensajes, formato (usuario, marca tiempo, mensaje)
+    """
+    message_and_authors = []
+    for friend in session['friends']:
+        texts = load_messages_from_user(friend)
+        message_and_authors.extend(texts)
+    return message_and_authors
+
+
+def load_messages_from_user(user):
+    """
+    Obtiene todos los mensajes de un usuario
+    :param user: el usuario
+    :return: todos los mensajes publicados, formato (usuario, marca tiempo, mensaje)
+    """
+    file_path = os.path.join(SITE_ROOT, "data/", user)
+    if not os.path.isfile(file_path):
+        return []
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    messages_with_author = [(data["user_name"], message[0], message[1]) for message in data["messages"]]
+    return messages_with_author
+
+
+def get_all_users(user):
+    """
+    Obtienes los amigos de un usuario (parameter)
+    :param user: usuario actual
+    :return: Lista de usuarios amigos del usuario actual
+    """
+    dir_path = os.path.join(SITE_ROOT, "data/")
+    user_list = listdir(dir_path)
+    user_list.remove(user)
+    return user_list
 
 #app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 # start the server with the 'run()' method
